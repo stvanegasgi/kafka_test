bd7f34c7ae64886169b690474f2bdff312234940
stvanegasgi <stvanegasgi@unal.edu.co>
2024-03-17 20:29:38 -0500

    add the function to delete and tranfer the files to directories of kafka

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import requests
 import os
+import shutil
 
 # =============================================================================
 # ============================== functions ====================================
 # =============================================================================
 
-def list_connectors(url_kafka_connect):
-    url_list_connectors = url_kafka_connect + "connectors"
+def list_connectors(url_kafka_connect, expand_info=False):
+    if (expand_info):
+        url_list_connectors = url_kafka_connect + "connectors"
+    else:
+        url_list_connectors = url_kafka_connect + "connectors?expand=info"
     response_create_connector = requests.get(url=url_list_connectors,
                                              headers={"Content-Type":"application/json"})
 
     if (response_create_connector.status_code in [200, 201]):
         return response_create_connector.json()
+    else:
+        return None
+
+def get_version_connector(url_kafka_connect):
+    url_get_connect_version = url_kafka_connect
+    response_get_connect_version = requests.get(url=url_get_connect_version,
+                                                headers={"Content-Type":"application/json"})
+    if (response_get_connect_version.status_code in [200, 201]):
+        return response_get_connect_version.json()
     else:
         return None
 
@@ -47,12 +60,39 @@ def create_connector(url_kafka_connect, data):
         return response_create_connector.json()
     else:
         return None
+
+def transfer_files_kafka_directories(list_path_files_origin, list_path_files_destination):
+    if (len(list_path_files_origin) != len(list_path_files_destination)):
+        return None
+    try:
+        for index_i, path_i in enumerate(list_path_files):
+            shutil.copy2(path_i, list_path_files_destination)
+        return True
+    except Exception as e:
+        print(f"--> error in transfer files to kafka --> '{e}'")
+        return None
+
+def delete_connector(url_kafka_connect, name_connector):
+    url_delete_connector_kafka = url_kafka_connect + f"connectors/{name_connector}/"
+
+    response_delete_connector = requests.delete(url=url_delete_connector_kafka,
+                                                headers={"Content-Type":"application/json"})
+
+    print("delete_connector ", response_delete_connector)
+    print("delete_connector ", response_delete_connector.json())
+
+    if (response_delete_connector.status_code in [200, 201, 204]):
+        return response_delete_connector.json()
+    else:
+        return None
+
 # =============================================================================
 # ============================== variables ====================================
 # =============================================================================
 
 origin_files_path      = os.getcwd() + "/csv_files/"
-data_volume_kafka_path = os.getcwd() + "/data/"
+data_volume_kafka_path = "/data/"
+data_volume_host       = os.getcwd() + "/data/"
 
 order_files = ['file_00.csv',
                'file_01.csv',
@@ -65,9 +105,13 @@ url_kafka_connect = "http://localhost:8083/"
 
 connector_name = "CONNECTOR_00"
 topic_name     = "TOPIC_00"
-input_path     = data_volume_kafka_path + "unprocessed"
-output_path    = data_volume_kafka_path + "processed"
-error_path     = data_volume_kafka_path + "error"
+
+input_path_kafka  = data_volume_kafka_path + "unprocessed"
+output_path_kafka = data_volume_kafka_path + "processed"
+error_path_kafka  = data_volume_kafka_path + "error"
+input_path_host   = data_volume_host + "unprocessed"
+output_path_host  = data_volume_host + "processed"
+error_path_host   = data_volume_host + "error"
 
 # =============================================================================
 # ============================== create the kafka folders =====================
@@ -76,17 +120,21 @@ error_path     = data_volume_kafka_path + "error"
 names_directories = ["unprocessed", "processed", "error"]
 
 for directory_i in names_directories:
-    if not os.path.exists(data_volume_kafka_path + f"{directory_i}"):
-        os.mkdir(data_volume_kafka_path + f"{directory_i}")
+    if not os.path.exists(data_volume_host + f"{directory_i}"):
+        os.mkdir(data_volume_host + f"{directory_i}")
 
-response = list_connectors(url_kafka_connect)
-if (response != None):
-    print("List connectors --> ", response)
+response_list_connectors = list_connectors(url_kafka_connect, True)
+
+print("\nList connectors --> ", response_list_connectors)
+
+response_version_connect = get_version_connector(url_kafka_connect)
+
+print("\nVersion connector --> ", response_version_connect)
 
 struc_config = create_config_struc_connector(connector_name,
                                              topic_name,
-                                             input_path,
-                                             output_path,
-                                             error_path)
+                                             input_path_kafka,
+                                             output_path_kafka,
+                                             error_path_kafka)
 
 print("input_path --> ", struc_config)
